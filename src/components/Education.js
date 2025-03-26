916862037606cfe7d9e7d277b71a0aa4ef6b1a0e
Mathilde Chaumard <mathilde.chaumard@gmail.com>
2025-03-26 11:48:46 +0100

    MaJ cv

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import pdf from '../assets/2024-mchaum.pdf';
+import pdf from '../assets/2025-mchaum.pdf';
 import { useTranslation, Trans } from 'react-i18next';
 
 const Education = () => {
